e5963142b8ce263bb38169b804b2b0aa6a556577
ggogun@gmail.com <ggogun@gmail.com@c8e6cdba-4040-8b7e-08a3-99ac4761913e>
2012-02-13 19:57:16 +0000

    Try My Blog RSS

---
+++ YYGNotice/src/kr/ggogun/yygnotice/data/RSSItem.java
@@ -0,0 +1,9 @@
+package kr.ggogun.yygnotice.data;
+
+
+public class RSSItem {
+        public String title;
+        public String date;
+        public String link;
+        public String description;
+}
